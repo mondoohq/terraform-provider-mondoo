4787a666a64d385603e402cb61eee1c25317b821
Christoph Hartmann <chris@lollyrock.com>
2024-05-20 11:17:41 +0200

    ⭐️ AWS integration (#83)
    * ⭐️ AWS integration
    * 🧹 update mondoo schema
    * 🧹 remove region argument

---
+++ examples/resources/mondoo_integration_aws/resource.tf
@@ -0,0 +1,36 @@
+variable "mondoo_org" {
+  description = "Mondoo Organization"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+provider "mondoo" {}
+
+# Create a new space
+resource "mondoo_space" "my_space" {
+  name   = "AWS Terraform"
+  org_id = var.mondoo_org
+}
+
+# Setup the AWS integration
+resource "mondoo_integration_aws" "name" {
+  space_id = mondoo_space.my_space.id
+  name     = "AWS Integration"
+
+  credentials = {
+    key = {
+      access_key = var.aws_access_key
+      secret_key = var.aws_secret_key
+    }
+  }
+}
+
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/hashicorp/terraform-plugin-log v0.9.0
 	github.com/hashicorp/terraform-plugin-testing v1.7.0
 	github.com/stretchr/testify v1.9.0
-	go.mondoo.com/mondoo-go v0.0.0-20240507081602-aa7a34bcb66d
+	go.mondoo.com/mondoo-go v0.0.0-20240516194133-d6612b90fe7c
 )
 
 require (
