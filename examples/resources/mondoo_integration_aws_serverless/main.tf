d2f7aea90fa78c1e3690aa7ac9c6c3f3fdc701b7
Salim Afiune Maya <afiune@mondoo.com>
2024-10-22 18:22:52 +0200

    ✅ update resource: integration_aws_serverless
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

--- examples/resources/mondoo_integration_aws_serverless/main.tf
+++ examples/resources/mondoo_integration_aws_serverless/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mondoo = {
       source  = "mondoohq/mondoo"
-      version = ">= 0.4.0"
+      version = ">= 0.19"
     }
     aws = {
       source  = "hashicorp/aws"
