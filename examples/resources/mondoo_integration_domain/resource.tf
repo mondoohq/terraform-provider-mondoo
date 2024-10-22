873692e8f1d0fdfec7d1a5a82b9981f109f982c8
Salim Afiune Maya <afiune@mondoo.com>
2024-10-22 18:05:03 +0200

    ✅ update resource: integration_domain
    Signed-off-by: Salim Afiune Maya <afiune@mondoo.com>

--- examples/resources/mondoo_integration_domain/resource.tf
+++ examples/resources/mondoo_integration_domain/resource.tf
@@ -1,22 +1,10 @@
-variable "mondoo_org" {
-  description = "The Mondoo Organization ID"
-  type        = string
-}
-
 provider "mondoo" {
-  region = "us"
-}
-
-# Create a new space
-resource "mondoo_space" "domain_space" {
-  name   = "My Space Name"
-  org_id = var.mondoo_org
+  space = "hungry-poet-123456"
 }
 
 # Setup the Domain integration
 resource "mondoo_integration_domain" "domain_integration" {
-  space_id = mondoo_space.domain_space.id
-  host     = "mondoo.com"
-  https    = true
-  http     = false
+  host  = "mondoo.com"
+  https = true
+  http  = false
 }
