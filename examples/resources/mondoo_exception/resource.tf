2a5f48f552bef0716973e208d167717513defa7e
Matthias Theuermann <mtheuermann@infralovers.com>
2024-12-02 16:44:12 +0100

    fix: type assertion
    Signed-off-by: Matthias Theuermann <mtheuermann@infralovers.com>

--- examples/resources/mondoo_exception/resource.tf
+++ examples/resources/mondoo_exception/resource.tf
@@ -1,11 +1,11 @@
 provider "mondoo" {
-  space = "eu-pensive-diffie-562318"
+  space = "eu-heuristic-hawking-332652"
 }
 
 resource "mondoo_exception" "exception" {
-  valid_until = "2024-12-03"
+  # valid_until = "2024-12-03"
   justification = "This is a test exception"
   action ="DISABLE"
   # check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-x-content-type-options-nosniff"]
-  check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-x-content-type-options-nosniff"]
+  check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-content-security-policy"]
 }
