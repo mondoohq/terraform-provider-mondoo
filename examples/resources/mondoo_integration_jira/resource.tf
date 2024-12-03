8505ba5a782dc36890d4d8458b69a56401acd2d5
Matthias Theuermann <mtheuermann@infralovers.com>
2024-12-03 19:51:59 +0100

    feat: finished jira integration
    Signed-off-by: Matthias Theuermann <mtheuermann@infralovers.com>

---
+++ examples/resources/mondoo_integration_jira/resource.tf
@@ -0,0 +1,21 @@
+variable "jira_token" {
+  description = "The Jira API Token"
+  type        = string
+  sensitive   = true
+}
+
+provider "mondoo" {
+  space = "hungry-poet-123456"
+}
+
+# Setup the jira integration
+resource "mondoo_integration_jira" "jira_integration" {
+  name        = "My Jira Integration"
+  host        = "https://your-instance.atlassian.net"
+  email       = "jiraowner@email.com"
+  # default_project = "GTMS"
+  api_token   = var.jira_token
+  
+  auto_create = true
+  auto_close  = true
+}
