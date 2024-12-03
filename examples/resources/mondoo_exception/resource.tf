9c136f2c8f94002e7f60350307fe2ce958086cbb
Matthias Theuermann <mtheuermann@infralovers.com>
2024-12-03 10:01:42 +0100

    removed incorrect function
    Signed-off-by: Matthias Theuermann <mtheuermann@infralovers.com>

--- examples/resources/mondoo_exception/resource.tf
+++ examples/resources/mondoo_exception/resource.tf
@@ -1,11 +1,12 @@
 provider "mondoo" {
-  space = "eu-heuristic-hawking-332652"
+  space = "eu-practical-goldwasser-115737"
 }
 
 resource "mondoo_exception" "exception" {
+  scope_mrn = "//assets.api.mondoo.app/spaces/eu-practical-goldwasser-115737/assets/2phR0MlEtyxv1kknyk7nNXYjddW"
   # valid_until = "2024-12-03"
   justification = "This is a test exception"
   action ="DISABLE"
   # check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-x-content-type-options-nosniff"]
-  check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-content-security-policy"]
+  check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-x-content-type-options-nosniff"]
 }
