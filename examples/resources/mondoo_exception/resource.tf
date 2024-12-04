e91f28e84ddbae785eeea5d79f111dbe1f282f68
Martin Buchleitner <mabunixda@gmail.com>
2024-12-04 12:09:39 +0100

    fix: ease up dev workflow
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- examples/resources/mondoo_exception/resource.tf
+++ examples/resources/mondoo_exception/resource.tf
@@ -1,12 +1,27 @@
-provider "mondoo" {
-  space = "eu-practical-goldwasser-115737"
+variable "spaceId" {
+  type = string 
 }
 
+provider "mondoo" {
+  region = "eu"
+  space = var.spaceId
+}
+
+data "mondoo_assets" "assets_data" {
+  space_id = var.spaceId
+}
+
+locals { 
+  ssl_asset = [ for asset in data.mondoo_assets.assets_data.assets: asset if startswith(asset.name, "https") ]
+  assetId = one(local.ssl_asset).id
+}
+
+
 resource "mondoo_exception" "exception" {
-  scope_mrn = "//assets.api.mondoo.app/spaces/eu-practical-goldwasser-115737/assets/2phR0MlEtyxv1kknyk7nNXYjddW"
-  # valid_until = "2024-12-03"
-  justification = "This is a test exception"
-  action ="DISABLE"
-  # check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-x-content-type-options-nosniff"]
-  check_mrns = ["//policy.api.mondoo.app/queries/mondoo-http-security-x-content-type-options-nosniff"]
+  scope_mrn = "//assets.api.mondoo.app/spaces/${var.spaceId}/assets/${local.assetId}"
+  valid_until = "2024-12-12T09:33:46.206Z"
+  justification = "testing"
+  action ="SNOOZE"
+  // check_mrns = ["//policy.api.mondoo.app/queries/mondoo-tls-security-no-weak-block-cipher-modes"]
+   check_mrns = [ "//policy.api.mondoo.app/queries/mondoo-tls-security-mitigate-beast" ]
 }
