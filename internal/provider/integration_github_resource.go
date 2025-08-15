08d29c5ccf6d1dee275ac99cb732a7172e7618ff
Casey Clayton <claytoncasey01@gmail.com>
2025-08-15 11:27:37 -0400

    Add force_replace bool to github resource

--- internal/provider/integration_github_resource.go
+++ internal/provider/integration_github_resource.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"regexp"
 
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
@@ -37,6 +39,9 @@ type integrationGithubResourceModel struct {
 	// integration details
 	Mrn  types.String `tfsdk:"mrn"`
 	Name types.String `tfsdk:"name"`
+
+	// force replacement on next apply to refresh credentials
+	ForceReplace types.Bool `tfsdk:"force_replace"`
 
 	Owner      types.String `tfsdk:"owner"`
 	Repository types.String `tfsdk:"repository"`
@@ -128,6 +133,15 @@ func (r *integrationGithubResource) Schema(ctx context.Context, req resource.Sch
 				Required:            true,
 				Validators: []validator.String{
 					stringvalidator.LengthAtMost(250),
+				},
+			},
+			"force_replace": schema.BoolAttribute{
+				MarkdownDescription: "Set to true to force replacement on next apply, useful to refresh credentials when the current value cannot be read.",
+				Optional:            true,
+				Computed:            true,
+				Default:             booldefault.StaticBool(false),
+				PlanModifiers: []planmodifier.Bool{
+					boolplanmodifier.RequiresReplace(),
 				},
 			},
 			"owner": schema.StringAttribute{
