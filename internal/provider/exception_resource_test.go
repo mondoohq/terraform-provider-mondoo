daab85d81b6857f3f5568ddb9c37f9e89b1cd059
vj <victoria.daphnee.jeffrey@gmail.com>
2025-08-29 12:47:50 -0600

    🧹 fix exceptions test

--- internal/provider/exception_resource_test.go
+++ internal/provider/exception_resource_test.go
@@ -57,18 +57,6 @@ func testCreateException(spaceId string, spaceMrn string, action string) string 
 		resource "mondoo_policy_assignment" "cis_policy_assignment_enabled" {
 		space_id = "%s"
 		policies = [
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-10-l1-ce",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-10-l1-bl",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-11-l1-ce",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-11-l1-bl",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-server-2016-dc-level-1",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-server-2016-ms-level-1",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-server-2019-dc-level-1",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-server-2019-ms-level-1",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-server-2022-dc-level-1",
-			"//policy.api.mondoo.app/policies/cis-microsoft-windows-server-2022-ms-level-1",
-			"//policy.api.mondoo.app/policies/cis-microsoft-azure-windows-server-2019-dc-level-1",
-			"//policy.api.mondoo.app/policies/cis-microsoft-azure-windows-server-2019-ms-level-1",
 			"//policy.api.mondoo.app/policies/cis-microsoft-azure-windows-server-2022-dc-level-1",
 			"//policy.api.mondoo.app/policies/cis-microsoft-azure-windows-server-2022-ms-level-1",
 			"//policy.api.mondoo.app/policies/mondoo-edr-policy",
@@ -84,11 +72,8 @@ func testCreateException(spaceId string, spaceMrn string, action string) string 
 		action        = "%s"
 		valid_until = "2025-09-09"
 		check_mrns = [
-			"//policy.api.mondoo.app/queries/cis-microsoft-windows-10--18.10.42.5.1",
-			"//policy.api.mondoo.app/queries/cis-microsoft-windows-11--18.10.42.5.1",
-			"//policy.api.mondoo.app/queries/cis-microsoft-windows-server-2016--18.10.42.5.1",
-			"//policy.api.mondoo.app/queries/cis-microsoft-windows-server-2019--18.10.42.5.1",
-			"//policy.api.mondoo.app/queries/cis-microsoft-windows-server-2022--18.10.42.5.1",
+			"//policy.api.mondoo.app/policies/cis-microsoft-azure-windows-server-2022-dc-level-1",
+			"//policy.api.mondoo.app/policies/cis-microsoft-azure-windows-server-2022-ms-level-1",
 		]
 		depends_on = [
 			mondoo_policy_assignment.cis_policy_assignment_enabled
