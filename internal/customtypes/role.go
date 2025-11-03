6cbcb3c44b9cf4a2c25a4e10a68bfec00e6e0d02
Jay Mundrawala <jay@mondoo.com>
2025-11-03 06:44:19 -0600

    cleanup

--- internal/customtypes/role.go
+++ internal/customtypes/role.go
@@ -20,8 +20,6 @@ const roleMRNPrefix = "//iam.api.mondoo.app/roles/"
 var (
 	_ basetypes.StringTypable                    = RoleType{}
 	_ basetypes.StringValuableWithSemanticEquals = RoleValue{}
-	_ basetypes.ListTypable                      = RoleListType{}
-	_ basetypes.ListValuableWithSemanticEquals   = RoleListValue{}
 )
 
 // RoleType represents a Mondoo IAM role that can be specified as either
@@ -81,13 +79,13 @@ type RoleValue struct {
 	basetypes.StringValue
 }
 
-// func (v RoleValue) Equal(o attr.Value) bool {
-// 	other, ok := o.(RoleValue)
-// 	if !ok {
-// 		return false
-// 	}
-// 	return v.StringValue.Equal(other.StringValue)
-// }
+func (v RoleValue) Equal(o attr.Value) bool {
+	other, ok := o.(RoleValue)
+	if !ok {
+		return false
+	}
+	return v.StringValue.Equal(other.StringValue)
+}
 
 func (v RoleValue) Type(ctx context.Context) attr.Type {
 	return RoleType{}
@@ -162,186 +160,4 @@ func NewRoleUnknown() RoleValue {
 	return RoleValue{
 		StringValue: basetypes.NewStringUnknown(),
 	}
-}
-
-// RoleListType is a custom list type for Mondoo IAM roles with semantic equality.
-type RoleListType struct {
-	basetypes.ListType
-}
-
-func (t RoleListType) Equal(o attr.Type) bool {
-	other, ok := o.(RoleListType)
-	if !ok {
-		return false
-	}
-	return t.ListType.Equal(other.ListType)
-}
-
-func (t RoleListType) String() string {
-	return "RoleListType"
-}
-
-func (t RoleListType) ValueFromList(ctx context.Context, in basetypes.ListValue) (basetypes.ListValuable, diag.Diagnostics) {
-	value := RoleListValue{
-		ListValue: in,
-	}
-	return value, nil
-}
-
-func (t RoleListType) ValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
-	attrValue, err := t.ListType.ValueFromTerraform(ctx, in)
-	if err != nil {
-		return nil, err
-	}
-
-	listValue, ok := attrValue.(basetypes.ListValue)
-	if !ok {
-		return nil, fmt.Errorf("unexpected value type of %T", attrValue)
-	}
-
-	listValuable, diags := t.ValueFromList(ctx, listValue)
-	if diags.HasError() {
-		return nil, fmt.Errorf("unexpected error converting ListValue to ListValuable: %v", diags)
-	}
-
-	return listValuable, nil
-}
-
-func (t RoleListType) ValueType(ctx context.Context) attr.Value {
-	return RoleListValue{}
-}
-
-// RoleListValue is a custom list value type for Mondoo IAM roles.
-type RoleListValue struct {
-	basetypes.ListValue
-}
-
-func (v RoleListValue) Type(ctx context.Context) attr.Type {
-	return RoleListType{
-		ListType: basetypes.ListType{
-			ElemType: RoleType{},
-		},
-	}
-}
-
-func (v RoleListValue) Equal(o attr.Value) bool {
-	other, ok := o.(RoleListValue)
-	if !ok {
-		return false
-	}
-	return v.ListValue.Equal(other.ListValue)
-}
-
-// ListSemanticEquals compares two role lists semantically.
-// Each role in the list is compared using normalized values.
-func (v RoleListValue) ListSemanticEquals(ctx context.Context, newValuable basetypes.ListValuable) (bool, diag.Diagnostics) {
-	var diags diag.Diagnostics
-
-	newValue, ok := newValuable.(RoleListValue)
-	if !ok {
-		diags.AddError(
-			"Semantic Equality Check Error",
-			"An unexpected value type was received while performing semantic equality checks.",
-		)
-		return false, diags
-	}
-
-	// Get the elements from both lists
-	oldElements := v.Elements()
-	newElements := newValue.Elements()
-
-	// Lists must have the same length
-	if len(oldElements) != len(newElements) {
-		return false, diags
-	}
-
-	// Compare each element semantically
-	for i := 0; i < len(oldElements); i++ {
-		oldRole, ok := oldElements[i].(RoleValue)
-		if !ok {
-			// Try to convert from StringValue
-			if oldStr, ok := oldElements[i].(basetypes.StringValue); ok {
-				oldRole = RoleValue{StringValue: oldStr}
-			} else {
-				diags.AddError(
-					"Semantic Equality Check Error",
-					fmt.Sprintf("Element at index %d is not a RoleValue", i),
-				)
-				return false, diags
-			}
-		}
-
-		newRole, ok := newElements[i].(RoleValue)
-		if !ok {
-			// Try to convert from StringValue
-			if newStr, ok := newElements[i].(basetypes.StringValue); ok {
-				newRole = RoleValue{StringValue: newStr}
-			} else {
-				diags.AddError(
-					"Semantic Equality Check Error",
-					fmt.Sprintf("Element at index %d is not a RoleValue", i),
-				)
-				return false, diags
-			}
-		}
-
-		// Compare normalized values
-		if normalizeRoleMRN(oldRole.ValueString()) != normalizeRoleMRN(newRole.ValueString()) {
-			return false, diags
-		}
-	}
-
-	return true, diags
-}
-
-// NewRoleListValue creates a new RoleListValue from a list of role strings.
-func NewRoleListValue(roles []string) RoleListValue {
-	elements := make([]attr.Value, len(roles))
-	for i, role := range roles {
-		elements[i] = NewRoleValue(role)
-	}
-
-	listValue, _ := basetypes.NewListValue(RoleType{}, elements)
-	return RoleListValue{
-		ListValue: listValue,
-	}
-}
-
-// NewRoleListNull creates a new null RoleListValue.
-func NewRoleListNull() RoleListValue {
-	return RoleListValue{
-		ListValue: basetypes.NewListNull(RoleType{}),
-	}
-}
-
-// NewRoleListUnknown creates a new unknown RoleListValue.
-func NewRoleListUnknown() RoleListValue {
-	return RoleListValue{
-		ListValue: basetypes.NewListUnknown(RoleType{}),
-	}
-}
-
-// NewRoleListValueFromList creates a RoleListValue from a basetypes.ListValue.
-// This is useful when converting API responses.
-func NewRoleListValueFromList(ctx context.Context, listValue basetypes.ListValue) (RoleListValue, error) {
-	// Extract string values from the list
-	var stringValues []string
-	diags := listValue.ElementsAs(ctx, &stringValues, false)
-	if diags.HasError() {
-		return RoleListValue{}, fmt.Errorf("failed to extract elements: %v", diags)
-	}
-
-	// Convert to RoleValue elements
-	elements := make([]attr.Value, len(stringValues))
-	for i, str := range stringValues {
-		elements[i] = NewRoleValue(str)
-	}
-
-	// Create the list
-	newList, diag := basetypes.NewListValue(RoleType{}, elements)
-	if diag.HasError() {
-		return RoleListValue{}, fmt.Errorf("failed to create list: %v", diag)
-	}
-
-	return RoleListValue{ListValue: newList}, nil
 }
